cbbeda14426ff9744f9fe31d7ecba403102c5089
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2025-09-04 14:55:55 +0300

    Добавлен второй сервис и обновлён первый

--- SwaggerUI/swagger-initializer.js
+++ SwaggerUI/swagger-initializer.js
@@ -1,10 +1,25 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  // Первый Swagger UI
   window.ui = SwaggerUIBundle({
     url: "../Service1.yaml",
     dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  // Второй Swagger UI
+  window.ui2 = SwaggerUIBundle({
+    url: "../Service2.yaml", // Укажите путь к вашему второму YAML файлу
+    dom_id: '#swagger-ui-2', // Уникальный идентификатор для второго контейнера
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
