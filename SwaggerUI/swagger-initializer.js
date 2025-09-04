8fb1b9a93d1dba85a5ac923635abf2be093d93a1
edshPC <ed_shh@mail.ru>
2025-09-04 15:07:23 +0300

    change initializer

--- SwaggerUI/swagger-initializer.js
+++ SwaggerUI/swagger-initializer.js
@@ -1,25 +1,13 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // Первый Swagger UI
+  // Swagger UI
   window.ui = SwaggerUIBundle({
-    url: "../Service1.yaml",
+    urls: [
+      { url: "../Service1.yaml", name: "Service1" },
+      { url: "../Service2.yaml", name: "Service2" },
+    ],
     dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
-
-  // Второй Swagger UI
-  window.ui2 = SwaggerUIBundle({
-    url: "../Service2.yaml", // Укажите путь к вашему второму YAML файлу
-    dom_id: '#swagger-ui-2', // Уникальный идентификатор для второго контейнера
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
